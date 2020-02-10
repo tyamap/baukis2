e5d9ebf7fa2080757ec16c01458b32fb04e10ed2
tyamap <tom333my@outlook.com>
2020-02-10 14:05:13 +0000

    add helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
-module ApplicationHelper
+module ApplicationHelper¥def document_title
+  
+  def dicument_title
+    if @title.present?
+      '#{@title} - Baukis2'
+    else
+      'Baukis2'
+    end
+  end
 end

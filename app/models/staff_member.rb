c4841706c01fc5953e1e637c120e29afd4d66cbb
tyamap <tom333my@outlook.com>
2020-02-11 11:35:47 +0000

    minor fixed

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,4 +1,5 @@
 class StaffMember < ApplicationRecord
+  # パスワードを文字列で受け取りハッシュ化する
   def password=(raw_password)
     if raw_password.kind_of?(String)
       self.hashed_password = BCrypt::Password.create(raw_password)

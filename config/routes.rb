6b6c54046aa9bbb0a423dba77eb08cb36e3b9e5b
tyamap <tom333my@outlook.com>
2020-02-12 14:35:32 +0000

    add resources-routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,16 @@ Rails.application.routes.draw do
     get "login" => "sessions#new", as: :login
     post "session" => "sessions#create", as: :session
     delete "session" => "sessions#destroy"
+
+    # get "staff_members" => "staff_members#index", as: :admin_staff_members
+    # get "staff_members/:id" => "staff_members#show", as: :admin_staff_member
+    # get "staff_members/new" => "staff_members#new", as: :new_admin_staff_member
+    # get "staff_members/:id/edit" => "staff_members#edit", as: :edit_admin_staff_member
+    # post "staff_members" => "staff_members#create"
+    # patch "staff_members/:id" => "staff_members#update"
+    # delete "staff_members/:id" => "staff_members#destroy"
+    # 以上７つの基本アクションを一括で設定できるのが resources メソッド。
+    resources :staff_members
   end
 
   namespace :customer do

c5f1413c30318ff5387b61979fe8c16d19d65233
tyamap <tom333my@outlook.com>
2020-02-13 12:34:27 +0000

    add routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :staff do
     root "top#index"
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+    # post "session" => "sessions#create", as: :session
+    # delete "session" => "sessions#destroy"
+    resource :session, only: [ :create, :destroy ]
+
+    # 単数リソース（自分自身に対する管理機能）の６つの基本アクションを一括で設定する resource メソッド
+    # 自分でアカウントを登録・追加する機能を除外
+    resource :account, except: [ :new, :create, :destroy ]
   end
 
   namespace :admin do
     root "top#index"
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+    # post "session" => "sessions#create", as: :session
+    # delete "session" => "sessions#destroy"
+    resource :session, only: [ :create, :destroy ]
 
     # get "staff_members" => "staff_members#index", as: :admin_staff_members
     # get "staff_members/:id" => "staff_members#show", as: :admin_staff_member

847f2b20dbad7edc00290d21fbd3367f3b8d0f10
tyamap <tom333my@outlook.com>
2020-02-13 13:17:22 +0000

    create routing-spec

--- config/routes.rb
+++ config/routes.rb
@@ -14,18 +14,7 @@ Rails.application.routes.draw do
     namespace :admin, path: config[:admin][:path] do
       root "top#index"
       get "login" => "sessions#new", as: :login
-      # post "session" => "sessions#create", as: :session
-      # delete "session" => "sessions#destroy"
       resource :session, only: [ :create, :destroy ]
-
-      # get "staff_members" => "staff_members#index", as: :admin_staff_members
-      # get "staff_members/:id" => "staff_members#show", as: :admin_staff_member
-      # get "staff_members/new" => "staff_members#new", as: :new_admin_staff_member
-      # get "staff_members/:id/edit" => "staff_members#edit", as: :edit_admin_staff_member
-      # post "staff_members" => "staff_members#create"
-      # patch "staff_members/:id" => "staff_members#update"
-      # delete "staff_members/:id" => "staff_members#destroy"
-      # 以上７つの基本アクションを一括で設定できるのが resources メソッド。
       resources :staff_members
     end
   end
---
+++ spec/routing/hostname_constraints_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+describe "Routing" do
+  it "get staff top-page" do
+    config = Rails.application.config.baukis2
+    url = "http://#{config[:staff][:host]}/#{config[:staff][:path]}"
+    expect(get: url).to route_to(
+      host: config[:staff][:host],
+      controller: "staff/top",
+      action: "index"
+    )
+  end
+
+  it "get admin login-form" do
+    config = Rails.application.config.baukis2
+    url = "http://#{config[:admin][:host]}/#{config[:admin][:path]}/login"
+    expect(get: url).to route_to(
+      host: config[:admin][:host],
+      controller: "admin/sessions",
+      action: "new"
+    )
+  end
+
+  it "is not routable if the host-name is disable" do
+    expect(get: "http:foo.example.jp").not_to be_routable
+  end
+
+  it "is not routable if the path does not exist" do
+    config = Rails.application.config.baukis2
+    expect(get: "http://#{config[:staff][:host]}/xyz").not_to be_routable
+  end
+end

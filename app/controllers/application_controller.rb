5d09bab7b6c88bdbaa284d90f04a97789b6fe863
tyamap <tom333my@outlook.com>
2020-02-15 14:19:27 +0000

    mod dev_env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   class Forbidden < ActionController::ActionControllerError; end
   class IpAddressRejected < ActionController::ActionControllerError; end
 
-  include ErrorHundlers
+  include ErrorHundlers if Rails.env.production?
 
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})

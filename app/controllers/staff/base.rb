37a984152ae83bcde4f58c93a589090009626101
tyamap <tom333my@outlook.com>
2020-02-16 06:19:43 +0000

    add access-controll

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,8 +1,17 @@
 class Staff::Base < ApplicationController
+  before_action :authorize
+
   private def current_staff_member
     if session[:staff_member_id]
       @current_staff_member ||=
         StaffMember.find_by(id: session[:staff_member_id])
+    end
+  end
+
+  private def authorize
+    unless current_staff_member
+      flash.alert = "職員としてログインしてください。"
+      redirect_to :staff_login
     end
   end
 

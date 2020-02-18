12c5454775341a7f0906841d73b7626e3787f5a2
tyamap <tom333my@outlook.com>
2020-02-18 12:39:49 +0000

    refactored

--- app/presenters/staff_member_form_presenter.rb
+++ app/presenters/staff_member_form_presenter.rb
@@ -7,7 +7,7 @@ class StaffMemberFormPresenter < FormPresenter
 
   def full_name_block(name1, name2, label_text, options = {})
     markup(:div, class: "input-block") do |m|
-      m << decorated_label(name, label_text, options)
+      m << decorated_label(name1, label_text, options)
       m << text_field(name1, options)
       m << text_field(name2, options)
     end

ece2f41ac5054c82b2b8c1eea68532ffc54306e4
tyamap <tom333my@outlook.com>
2020-02-20 12:42:19 +0000

    create customer-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-table_name = %w(staff_members administrators staff_events)
+table_name = %w(staff_members administrators staff_events, customers)
 
 table_name.each do |table_name|
   path = Rails.root.join("db", "seeds", Rails.env, "#{table_name}.rb")

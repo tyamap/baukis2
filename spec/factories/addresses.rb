114c09f73874c1164b99f017ebdb11849a1c4cf8
tyamap <tom333my@outlook.com>
2020-02-24 11:52:05 +0000

    fix

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -3,17 +3,17 @@ FactoryBot.define do
     postal_code { "1000000" }
     prefecture { "東京都" }
     city { "千代田区" }
-    address1 { "試験 1-1-1" }
+    address1 { "試験1-1-1" }
     address2 { "" }
   end
 
   factory :work_address do
     company_name { "テスト" }
-    division_name{ "開発部" }
+    division_name { "開発部" }
     postal_code { "1050000" }
     prefecture { "東京都" }
     city { "港区" }
-    address1 { "試験 1-1-1" }
+    address1 { "試験1-1-1" }
     address2 { "" }
   end
 end
--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     family_name_kana { "ヤマダ" }
     given_name_kana { "タロウ" }
     password { "pw" }
-    birthday { Date.new(1970,1,1) }
+    birthday { Date.new(1970, 1, 1) }
     gender { "male" }
     association :home_address, strategy: :build
     association :work_address, strategy: :build

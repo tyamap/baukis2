0dc46c3f8f526b9fa003b6813861e50b08fa6c0b
tyamap <tom333my@outlook.com>
2020-02-25 13:43:56 +0000

    create common-modules

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,22 +1,10 @@
 class StaffMember < ApplicationRecord
-include StringNormalizer
+  include EmailHolder
+  include PasswordHolder
+  include PersonalNameHolder
 
   has_many :events, class_name: "StaffEvent", dependent: :destroy
 
-  before_validation do
-    self.email = normalize_as_email(email)
-    self.family_name = normalize_as_name(family_name)
-    self.given_name = normalize_as_name(given_name)
-    self.family_name_kana = normalize_as_furigana(family_name_kana)
-    self.given_name_kana = normalize_as_furigana(given_name_kana)
-  end
-
-  HUMAN_NAME_REGEXP = /\A[\p{han}\p{hiragana}\p{katakana}\u{30fc}A-Za-z]+\z/
-  KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
-
-  validates :email, presence: true, "valid_email_2/email": true, uniqueness: { case_sensitive: false }
-  validates :family_name, :given_name, presence: true, format: { with: HUMAN_NAME_REGEXP, allow_blank: true }
-  validates :family_name_kana, :given_name_kana, presence: true, format: { with: KATAKANA_REGEXP, allow_blank:true }
   validates :start_date, presence: true, date: {
     after_or_equal_to: Date.new(2000, 1, 1),
     before: ->(_obj) { 1.year.from_now.to_date },
@@ -27,15 +15,6 @@ include StringNormalizer
     before: ->(_obj) { 1.year.from_now.to_date },
     allow_blank: true
   }
-
-  # パスワードを文字列で受け取りハッシュ化する
-  def password=(raw_password)
-    if raw_password.kind_of?(String)
-      self.hashed_password = BCrypt::Password.create(raw_password)
-    elsif raw_password.nil?
-      self.hashed_password = nil
-    end
-  end
 
   def active?
     !suspended && start_date <= Date.today &&
